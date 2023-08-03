7f4d2ae78a3ad21296c98f82548309da9f8dd7ad
ajmaestre <ajmaestretorres@gmail.com>
2023-08-03 14:22:15 -0500

    version 1.3.8

--- routes/juradoRoute.php
+++ routes/juradoRoute.php
@@ -18,6 +18,7 @@
     if($_SERVER["REQUEST_METHOD"] == "GET"){
 
         $headers = getallheaders();
+        echo $headers["Token"];
         foreach ($headers as $name => $value) {
             echo "$name: $value ";
         }
