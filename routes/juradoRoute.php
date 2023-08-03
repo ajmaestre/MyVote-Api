39f298865cad048f7ac92f65bb04e0ba1383685f
ajmaestre <ajmaestretorres@gmail.com>
2023-08-03 12:11:59 -0500

    version 1.2.9

--- routes/juradoRoute.php
+++ routes/juradoRoute.php
@@ -9,6 +9,11 @@
     $respuesta = new Respuesta;
     $auth = new authClass;
 
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: Access-Control-Allow-Origin, Content-Type, token");
+    header("Content-Type: application/json");
+
 
     if($_SERVER["REQUEST_METHOD"] == "GET"){
 
